de5bffe851f5678ce2895b933dc15186c06c611c
Toby Mao <maohanyang789@163.com>
2018-09-27 22:04:21 +0800

    tempory remove static files

--- myapp/app.js
+++ myapp/app.js
@@ -18,10 +18,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+//app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
