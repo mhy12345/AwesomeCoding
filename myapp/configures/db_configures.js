90cc6231d44e4d1b2279e2105b1eb9acbac078d4
Toby Mao <maohanyang789@163.com>
2018-09-29 14:37:26 +0800

    update api showtables&doquery

--- myapp/configures/db_configures.js
+++
@@ -1,8 +0,0 @@
-var configures = {
-  host     : 'Database.fantastic67.secoder.local',
-  user     : 'root',
-  password : 'awesome-coding',
-  database : 'ac_database'
-}
-
-module.exports = configures
--- myapp/routes/api.js
+++ myapp/routes/api.js
@@ -2,40 +2,39 @@ var express = require('express');
 var router = express.Router();
 var get_connection = require('../utils/database');
 
-router.get('/show_table', function(req, res, next) { //在数据库中查找表格，并打印
-	var sql = 'SELECT * from ' + req.query.table_name;
-	var text = '>>>>> ' + sql + '<br/>';
+function do_sql_query(sql,callback) {
+	var result = {};
+	result.query = sql;
+	result.results = [];
+	result.status = 'SUCCESS.';
 	get_connection(function(conn) {
 		conn.query(sql, function (error, results, fields) {
 			if (error) {
-				text += "ERROR ... " + error;
-				res.send(text);
+				result.status = 'FAILED.'
+				result.details = error;
+				callback(result);
 			}else {
-				//console.log(results);
 				for (var i=0;i<results.length;i++) {
-					text += JSON.stringify(results[i]) + "<br/>";
+					result.results.push(results[i]);
 				}
-				res.send(text);
+				callback(result);
 			}
 		});
+	});
+}
+
+router.get('/show_table', function(req, res, next) { //在数据库中查找表格，并打印
+	var sql = 'SELECT * FROM ' + req.query.table_name;
+	do_sql_query(sql,function(result) {
+		res.send(JSON.stringify(result,null,3));
 	});
 });
 
 router.get('/do_query', function(req,res,next) { //在数据库中执行指定的SQL命令
 	var sql = req.query.sql;
-	get_connection(function(conn) {
-		conn.query(sql, function (error, results, fields) {
-			//console.log("DO QUERY : "+sql);
-			if (error) {
-				res.send(error);
-				//console.log("FAILED");
-			}else {
-				res.send("Success.");
-				//console.log("SUCCESS");
-			}
-		});
+	do_sql_query(sql,function(result) {
+		res.send(JSON.stringify(result,null,3));
 	});
-
 });
 
 module.exports = router;
