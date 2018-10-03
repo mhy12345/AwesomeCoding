ffc387e200edd12fbc8d88b38ac41395c884399f
郑逢时 <2016012177@secoder.net>
2018-10-03 12:05:59 +0800

    no message

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,36 @@
-<!--前端整体布局-->
 <template>
-    <div id="app">
-        <router-link to="/">主页</router-link>  |
-        <router-link to="/dataVisualizer">数据库</router-link>  |
-        <router-link to="/about">关于</router-link>
-        <h1>欢迎进入AwesomeCoding的前端</h1>
-        <br>
-        <router-view></router-view>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
     </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-    name: 'app',
-    data() {
-        return {}
-    },
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
 <style>
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
