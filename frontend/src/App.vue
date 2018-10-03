b36be6cf016cb29d6ddf8b735eee251801183f4d
郑逢时 <2016012177@secoder.net>
2018-10-03 12:12:30 +0800

    installed all element plugins

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,18 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
+    <div id="app">
+        <router-link to="/">主页</router-link>  |
+        <router-link to="/dataVisualizer">数据库</router-link>  |
+        <router-link to="/about">关于</router-link>
+        <h1>欢迎进入AwesomeCoding的前端</h1>
+        <br>
+        <router-view></router-view>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'app',
+};
 </script>
 
 <style>
