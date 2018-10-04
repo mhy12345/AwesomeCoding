3b785ecd3dffc885a0a0eb4a354e4db194d3ded2
郑逢时 <2016012177@secoder.net>
2018-10-04 10:43:58 +0800

    no message

--- myapp/utils/database.js
+++ myapp/utils/database.js
@@ -1,25 +1,26 @@
-var mysql      = require('mysql');
+var mysql      	 = require('mysql');
 var mysql_config = require('../configures/db_configures');
-var mysql_init = require('./mysql_initializer');
+var mysql_init   = require('./mysql_initializer');
 
 function get_connection(callback) { //获取连接connection，并调用回调函数
-	console.log(mysql_config);
-	connection = mysql.createConnection(mysql_config);
-	connection.connect(function (err) {
-		if (err) {
-			if (err.code == 'ER_BAD_DB_ERROR') {
-				console.log("BAD DATABASE!");
-				mysql_init(callback);
-				return;
-			}else {
-				console.log("UNPREDICTED ERROR!");
-				mysql_init(callback);
-				return;
-			}
-		}
-		console.log("Successfully connect to database...");
-		callback(connection);
-	});
+    console.log(mysql_config);
+    connection = mysql.createConnection(mysql_config);
+    connection.connect(function (err) {
+        if (err) {
+            if (err.code == 'ER_BAD_DB_ERROR') {
+                console.log("BAD DATABASE!");
+                mysql_init(callback);
+                return;
+            }
+            else {
+                console.log("UNPREDICTED ERROR!");
+                mysql_init(callback);
+                return;
+            }
+        }
+        console.log("Successfully connect to database...");
+        callback(connection);
+    });
 }
 
-module.exports = get_connection
+module.exports = get_connection;
