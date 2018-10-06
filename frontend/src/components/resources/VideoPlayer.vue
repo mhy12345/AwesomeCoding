904b9f8d65f38d4648f3a3d89c8e688bdc7e90cf
Yueqing Jia <yueqingjia@Yueqings-MacBook-Pro.local>
2018-10-06 11:03:26 +0800

    hotfix

--- frontend/src/components/resources/VideoPlayer.vue
+++ frontend/src/components/resources/VideoPlayer.vue
@@ -5,14 +5,18 @@
 </template>
 
 <script>
+    import polyvPlayer from 'polyvPlayer'
+
     export default {
         mounted: function() {
+            // eslint-disable-next-line
             var player = polyvPlayer({
                 wrap: '#player',
                 width: 800,
                 height: 533,
                 vid: '047a911d833fd4ec0a7f4bbdea2a7b93_0',
             });
+
         },
         data() {
         }
