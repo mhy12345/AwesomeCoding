d62ba363a3d280fe3300cc51e1ce86e7ddc3a48c
liuzhiqiang <584324893@qq.com>
2018-10-10 14:26:01 +0800

    defeat injection attack

--- myapp/routes/developer.js
+++ myapp/routes/developer.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var funcs = require('../utils/funcs');
 var do_sql_query = funcs.do_sql_query;
+var mysql=require('mysql');
 
 router.get('/show_table', function(req, res, next) { //在数据库中查找表格，并打印
-	var sql = 'SELECT * FROM ' + req.query.table_name;
+	var sql = 'SELECT * FROM ' + mysql.escapeId(req.query.table_name);
 	do_sql_query(sql, function (result) {
 		res.send(JSON.stringify(result, null, 3));
 	});
@@ -12,7 +13,7 @@ router.get('/show_table', function(req, res, next) { //在数据库中查找表�
 
 router.get('/show_columns', function(req, res, next) {
 	var mysql_config = require('../configures/db_configures');
-	var db_name = mysql_config.database;
+	var db_name = mysql.escapeId(mysql_config.database);
 	var sql = 'SELECT (COLUMN_NAME) FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = \''
 		+ db_name + '\' AND TABLE_NAME = \'' + req.query.table_name + '\'';
     do_sql_query(sql, function (result) {
