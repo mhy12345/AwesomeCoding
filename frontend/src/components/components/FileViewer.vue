b40ffb5313317f0c8dbc5c9caf04db900b0a2916
Toby Mao <maohanyang789@163.com>
2018-10-10 10:11:27 +0800

    [update] Update File Viewer

--- frontend/src/components/components/FileViewer.vue
+++ frontend/src/components/components/FileViewer.vue
@@ -1,26 +1,26 @@
 <template>
-	<el-container>
-		<el-aside>
-			<div>
-			</div>
-		</el-aside>
-		<el-main>
-			<div class='embed-pdf'>
-				<pdf 
-				 id = 'pdf-frame'
-				 :page='page'
-				 :src='pdf_src'
-				 ref='pdf'
-				 >
-				</pdf>
-				<el-row>
-				  <el-button plain @click='prevPage'>上一页</el-button>
-				  <span>第{{page}}页</span>
-				  <el-button plain @click='nextPage'>下一页</el-button>
-				</el-row>
-			</div>
-		</el-main>
-	</el-container>
+	<el-row>
+		<el-col :span='6'>
+			asd
+		</el-col>
+		<el-col :span='18'>
+			<el-row type='flex' justify='center'>
+					<pdf 
+					id = 'pdf-frame'
+					:page='page'
+					:src='pdf_src'
+					@num-pages="page_num = $event"
+					ref='pdf'
+					>
+					</pdf>
+			</el-row>
+			<el-row type='flex' justify="center">
+					<el-button plain @click='prevPage' style='height:40px'>上一页</el-button>
+					<span style='height:40px;text-align:center;margin-top:8px'>第{{page}}页 / 共{{page_num}}页</span>
+					<el-button plain @click='nextPage' style='height:40px'>下一页</el-button>
+			</el-row>
+		</el-col>
+	</el-row>
 </template>
 
 <script>
@@ -32,23 +32,22 @@ export default {
 		return {
 			page : 1,
 			pdf_src:"https://cseweb.ucsd.edu/classes/fa13/cse160-a/Lectures/Lec07.pdf",
+			page_num : 0
 		}
 	},
 	components: {
 		pdf : pdf
 	},
 	mounted : function() {
-		console.log("MOUNTED");
-		this.$on('num-pages',function(num) {
-			console.log("PAGE NUM : ",num);
-		});
 	},
 	methods : {
 		nextPage : function() {
-			this.page += 1
+			if (this.page+1 <= this.page_num)
+				this.page += 1
 		},
 		prevPage : function() {
-			this.page -= 1
+			if (this.page-1>0)
+				this.page -= 1
 		}
 	}
 }
@@ -62,5 +61,11 @@ export default {
 }
 .annotationLayer {
 	display:none;
+}
+.page-panel{
+	margin:auto;
+}
+.margin-auto {
+	margin:auto;
 }
 </style>
