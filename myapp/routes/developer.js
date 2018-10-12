3a43d03e2ce7918cff3057e4c027282cf322ddfe
Toby Mao <maohanyang789@163.com>
2018-10-12 10:09:42 +0800

    [update] Rebuild database query handlers, update some of the APIs

--- myapp/routes/developer.js
+++ myapp/routes/developer.js
@@ -1,31 +1,57 @@
 var express = require('express');
 var router = express.Router();
-var funcs = require('../utils/funcs');
-var do_sql_query = funcs.do_sql_query;
+var {do_sql_query,get_connection} = require('../utils/funcs');
 var mysql=require('mysql');
 
 router.get('/show_table', function(req, res, next) { //在数据库中查找表格，并打印
-	var sql = 'SELECT * FROM ' + mysql.escapeId(req.query.table_name);
-	do_sql_query(sql, function (result) {
-		res.send(JSON.stringify(result, null, 3));
+	get_connection()
+	.then(function(conn) {
+		let sql = 'SELECT * FROM ' + mysql.escapeId(req.query.table_name);
+		return do_sql_query(conn,sql);
+	})
+	.then(function(packed) {
+		let {conn,sql_res} = packed;
+		conn.end();
+		res.send(JSON.stringify(sql_res, null, 3));
+	})
+	.catch(function(sql_res) {
+		res.send(JSON.stringify(sql_res, null, 3));
 	});
 });
 
 router.get('/show_columns', function(req, res, next) {
-	var mysql_config = require('../configures/db_configures');
-	var db_name = (mysql_config.database);
-	var sql = 'SELECT (COLUMN_NAME) FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '
-		+ mysql.escape(db_name) + ' AND TABLE_NAME = ' + mysql.escape(req.query.table_name) + '';
-    do_sql_query(sql, function (result) {
-        res.send(JSON.stringify(result, null, 3));
-    });
+	get_connection()
+	.then(function(conn) {
+		let mysql_config = require('../configures/db_configures');
+		let db_name = (mysql_config.database);
+		let sql = 'SELECT (COLUMN_NAME) FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '
+			+ mysql.escape(db_name) + ' AND TABLE_NAME = ' + mysql.escape(req.query.table_name) + '';
+		return do_sql_query(conn,sql);
+	})
+	.then(function(packed) {
+		let {conn,sql_res} = packed;
+		conn.end();
+        res.send(JSON.stringify(sql_res, null, 3));
+	})
+	.catch(function(sql_res) {
+        res.send(JSON.stringify(sql_res, null, 3));
+	});
 });
 
 
 router.get('/do_query', function (req, res, next) { //在数据库中执行指定的SQL命令
-	var sql = req.query.sql;
-	do_sql_query(sql, function (result) {
-		res.send(JSON.stringify(result, null, 3));
+	get_connection()
+	.then(function(conn) {
+		let sql = req.query.sql;
+		return do_sql_query(conn,sql);
+	})
+	.then(function(packed) {
+		let {conn,sql_res} = packed;
+		conn.end();
+		res.send(JSON.stringify(sql_res, null, 3));
+	})
+	.catch(function(sql_res) {
+		res.send(JSON.stringify(sql_res, null, 3));
 	});
 });
 
