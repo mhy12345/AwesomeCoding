76b24e236a53f8abdb1e7da86e7ff377e6834ffd
Toby Mao <maohanyang789@163.com>
2018-10-13 19:46:54 +0800

    [update] Remove unit-test for frontend

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -8,13 +8,16 @@ module.exports = {
   env: {
     browser: true,
   },
+	"extends": "eslint-config-airbnb",
+
+	/*
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential', 
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
-  ],
+  ],*/
   // required to lint *.vue files
   plugins: [
     'vue'
