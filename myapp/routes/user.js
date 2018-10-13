951f6784a40b7c0f602f315e2293917b1e4ad4ef
郑逢时 <2016012177@secoder.net>
2018-10-13 10:59:06 +0800

    [update] standardized a variable name in user.js.

--- myapp/routes/user.js
+++ myapp/routes/user.js
@@ -13,114 +13,111 @@ router.get('/session', function (req, res, next) {	// 判断用户是否登录
 
 router.post('/register', function (req, res, next) {	// 响应注册，并进行合法判断
     console.log("[post] register\n", req.body);
-    var resbody = {};
+    var res_body = {};
 	if (typeof(req.session.user_id) !== 'undefined') {  // 已登录
-		resbody = {
+		res_body = {
 			status : 'FAILED.',
 			details : 'ALREADY_LOGIN.'
 		};
-        console.log('[res]', resbody);
-        res.send(JSON.stringify(resbody));
+        console.log('[res]', res_body);
+        res.send(JSON.stringify(res_body));
         return;
 	}
-	get_connection().
-    then(function(conn) {
-			let sql = 'SELECT * FROM users WHERE nickname = ' + mysql.escape(req.body.nickname);
-			return do_sql_query(conn,sql);
-		})
-		.then(function(packed) {
-			let {conn,sql_res} = packed;
-			if (sql_res.results.length !== 0) {    //判重
-				conn.end();
-				return Promise.reject({
-					status: 'FAILED.',
-					details: 'DUPLICATION_OF_REGISTRATION.'
-				});
-			}
-			// 不重复
-			var values = [];
-			var items = ['id', 'nickname', 'realname', 'role', 'email', 'registration_date', 'password'];
-			for (var item of items) {
-				if (req.body[item] === undefined || req.body[item] === null || req.body[item] === '')
-					values.push('null');
-				else{
-					if(item === 'registration_date') {
-						values.values.push('\'' + Date.now() + '\'');
-					}
-					else {
-						values.push('\'' + req.body[item] + '\'');
-					}
-				}
-			}
-			for(var value of values) {
-				value = mysql.escape(value);
-			}
-            var sql = 'insert into users (' + items.join(',') + ') values (' + values.join(',') + ')';
-			return do_sql_query(conn,sql);
-		})
-		.then(function(packed) {
-			let {conn,sql_res} = packed;
-			resbody.status = "SUCCESS.";              // 成功注册
-			resbody.results = req.body;
-			req.session.islogin = true;
-			req.session.nickname = req.body.nickname;
-			req.session.realname = req.body.realname;
-			req.session.email = req.body.email;
-			req.session.motto = req.body.motto;
-			console.log('[res]', resbody);
-			conn.end();
-			res.send(JSON.stringify(resbody));
-		})
-		.catch(function(sql_res) {
-			res.send(JSON.stringify(sql_res));
-		});
+    get_connection().then(function (conn) {
+        let sql = 'SELECT * FROM users WHERE nickname = ' + mysql.escape(req.body.nickname);
+        return do_sql_query(conn, sql);
+    }).then(function (packed) {
+        let {conn, sql_res} = packed;
+        if (sql_res.results.length !== 0) {    //判重
+            conn.end();
+            return Promise.reject({
+                status: 'FAILED.',
+                details: 'DUPLICATION_OF_REGISTRATION.'
+            });
+        }
+        // 不重复
+        var values = [];
+        var items = ['id', 'nickname', 'realname', 'role', 'email', 'registration_date', 'password'];
+        for (var item of items) {
+            if (req.body[item] === undefined || req.body[item] === null || req.body[item] === '')
+                values.push('null');
+            else {
+                if (item === 'registration_date') {
+                    values.values.push('\'' + Date.now() + '\'');
+                }
+                else {
+                    values.push('\'' + req.body[item] + '\'');
+                }
+            }
+        }
+        for (var value of values) {
+            value = mysql.escape(value);
+        }
+        var sql = 'insert into users (' + items.join(',') + ') values (' + values.join(',') + ')';
+        return do_sql_query(conn, sql);
+    }).then(function (packed) {
+        let {conn, sql_res} = packed;
+        res_body.status = "SUCCESS.";              // 成功注册
+        res_body.results = req.body;
+        req.session.islogin = true;
+        req.session.nickname = req.body.nickname;
+        req.session.realname = req.body.realname;
+        req.session.email = req.body.email;
+        req.session.motto = req.body.motto;
+        console.log('[res]', res_body);
+        conn.end();
+        res.send(JSON.stringify(res_body));
+    }).
+    catch(function (sql_res) {
+        res.send(JSON.stringify(sql_res));
+    });
 });
 
 router.post('/login', function(req, res, next) {  // 响应登录，并进行合法判断 返回 JSON
 	console.log("[post] login\n", req.body);
 	let nickname = req.body.nickname;
 	let password = req.body.password;
-	get_connection()
-		.then(function(conn) {
-			let sql = 'SELECT * FROM users WHERE nickname = ' + mysql.escape(nickname);
-			return do_sql_query(conn,sql);
-		})
-		.then(function(packed) {
-			let {conn,sql_res} = packed;
-			if (sql_res.results.length === 0) {
-				conn.end();
-				return Promise.reject({
-					status: 'FAILED.',
-					details: 'USER_NOT_FOUND.'
-				});
-			}
-			else {
-				let user = sql_res.results[0];
-				console.log('Found:', user);
-				if (password !== user.password) {
-					conn.end();
-					return Promise.reject({
-						status : 'FAILED.',
-						details : 'WRONG_PASSWORD.'
-					});
-				}
-				else {
-					req.session.nickname = user.nickname;
-					req.session.realname = user.realname;
-					req.session.user_id = user.id;
-					req.session.email = user.email;
-					conn.end();
-					res.send({
-						status : 'SUCCESS.',
-						details : 'SUCCESS.',
-						results : user
-					});
-				}
-			}
-		})
-		.catch(function(sql_res) {
-			res.send(JSON.stringify(sql_res));
-		});
+    get_connection().
+    then(function (conn) {
+        let sql = 'SELECT * FROM users WHERE nickname = ' + mysql.escape(nickname);
+        return do_sql_query(conn, sql);
+    }).
+    then(function (packed) {
+        let {conn, sql_res} = packed;
+        if (sql_res.results.length === 0) {
+            conn.end();
+            return Promise.reject({
+                status: 'FAILED.',
+                details: 'USER_NOT_FOUND.'
+            });
+        }
+        else {
+            let user = sql_res.results[0];
+            console.log('Found:', user);
+            if (password !== user.password) {
+                conn.end();
+                return Promise.reject({
+                    status: 'FAILED.',
+                    details: 'WRONG_PASSWORD.'
+                });
+            }
+            else {
+                req.session.nickname = user.nickname;
+                req.session.realname = user.realname;
+                req.session.user_id = user.id;
+                req.session.email = user.email;
+                conn.end();
+                res.send({
+                    status: 'SUCCESS.',
+                    details: 'SUCCESS.',
+                    results: user
+                });
+            }
+        }
+    }).
+    catch(function (sql_res) {
+        res.send(JSON.stringify(sql_res));
+    });
 });
 
 router.get('/logout', function (req, res, next) {
