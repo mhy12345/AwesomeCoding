a6ad809adf7f87994c4a757432569d27b22c7384
Toby Mao <maohanyang789@163.com>
2018-10-18 10:28:01 +0800

    [changed] frontend helloworld.spec.js test file.

--- frontend/test/unit/specs/Helloworld.spec.js
+++ frontend/test/unit/specs/Helloworld.spec.js
@@ -1,6 +1,12 @@
-//import UnitTest from '@/components/components/UnitTest'
+import UnitTest from '@/components/components/UnitTest';
+import Vue from 'Vue';
 
 describe('UnitTest.vue', () => {
 	it('should render correct contents', () => {
+		const Constructor = Vue.extend(UnitTest);
+		const ListComponent = new Constructor().$mount();
+		expect(ListComponent.$el.textContent).to.contain('Welcome to Your Vue.js App');
+		ListComponent.msg = 'Wow, it changed.';
+		expect(ListComponent.$el.textContent).to.contain('Wow, it changed.');
 	})
 })
