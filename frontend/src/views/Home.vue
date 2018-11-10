b426774c530dbc242bc567ebca92dbca7071b2e6
Yueqing Jia <yueqingjia@Yueqings-MacBook-Pro.local>
2018-11-10 14:36:28 +0800

    [add]Course cards on the homepage, #98

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="block">
-        <el-carousel height="300px">
-            <el-carousel-item v-for="(currentitem, idx) in items">
-                <h3 v-if="currentitem.path === ''">
+        <el-carousel class="carousel_css">
+            <el-carousel-item v-for="(currentitem, idx) in headlines" class = "carousel_item">
+                <h3 v-if="currentitem.path === ''" @click="jump(currentitem)">
                     {{ currentitem.title }}
                 </h3>
                 <h3 v-else>
@@ -10,6 +10,19 @@
                 </h3>
             </el-carousel-item>
         </el-carousel>
+
+        <el-row class="fucked">
+            <el-col :span="4" v-for="(currentitem, idx) in headlines" class = "card_css">
+                <el-card :body-style="{ padding: '0px' }">
+                    <h3 v-if="currentitem.path === ''" @click="jump(currentitem)">
+                        {{ currentitem.title }}
+                    </h3>
+                    <div style="padding: 14px;">
+                        <span> {{currentitem.title}} </span>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
@@ -21,7 +34,7 @@
     export default {
         data: function () {
             return {
-                items: [],
+                headlines: [],
                 tableData: [],
                 title: ' ',
                 idx: 'publicCourses'
@@ -37,32 +50,48 @@
                      console.log(this.tableData);
 
                      for(let i = 0; i < 6; i ++) {
-                         this.items.push(
+                         this.headlines.push(
                              {
                                  title: this.tableData[i].title,
+                                 id: this.tableData[i].id,
                                  path: ''
                              }
                          );
                      }
-
-                     console.log(this.items);
                  }).
                  catch(function (res) {
                      this.$message(res);
                  });
         },
         methods: {
+            jump: function (currentitem) {
+                this.$router.push({name: 'class', params: {class_id: currentitem.id}});
+            }
         },
         components: {}
     };
 </script>
 
 <style>
+    .carousel_css {
+        margin: 20px;
+        height: 400px;
+    }
+    .carousel_item {
+        height: 400px;
+    }
+    .fucked {
+        margin: 20px;
+        height: 400px;
+    }
+    .card_css {
+        margin: 20px;
+    }
     .el-carousel__item h3 {
         color: #475669;
         font-size: 52px;
         margin: 0 auto;
-        line-height: 300px;
+        line-height: 400px;
         text-align: center;
     }
 
@@ -75,5 +104,13 @@
 
     .el-carousel__item:nth-child(2n+1) {
         background-color: #d3dce6;
+    }
+
+    .el-card h3 {
+        color: #475669;
+        font-size: 26px;
+        margin: 0 auto;
+        line-height: 150px;
+        text-align: center;
     }
 </style>
