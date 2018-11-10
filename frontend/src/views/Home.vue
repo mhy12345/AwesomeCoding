e42e4188d7ad0b372b918292f98bbc5c8080fced
Yueqing Jia <yueqingjia@Yueqings-MacBook-Pro.local>
2018-11-10 11:37:02 +0800

    [add]el-carousel in home page, #98

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,29 +1,73 @@
 <template>
-    <div class="home">
-        <h3>当前时间：{{cur_time}}</h3>
-        <HelloWorld msg="帮助文档"/>
+    <div class="block">
+        <el-carousel height="300px">
+            <el-carousel-item v-for="(currentimgpath, idx) in imgpath">
+                <h3 v-if="currentimgpath === ''">
+                    {{ itemtitle[idx] }}
+                </h3>
+                <h3 v-else>
+                    {{ idx }}
+                </h3>
+            </el-carousel-item>
+        </el-carousel>
     </div>
 </template>
 
 <script>
     /* eslint-disable camelcase */
 
-    // @ is an alias to /src
-    import HelloWorld from '../components/components/HelloWorld.vue';
+    import {supported_resources} from '../utils/CourseLists';
 
     export default {
         name: 'Home',
         data() {
-            return {cur_time: Date().toLocaleString(),};
+            return{
+                itemtitle: ['中国历史', '世界历史', '软件工程', '编译原理', 'e', 'f'],
+                imgpath: ['', '', '', '', '', ''],
+                items: ['a', 'b', 'c', 'd', 'e', 'f'],
+                tableData: [],
+                title: ' ',
+                isTeacher: false,
+                idx: 'publicCourses'
+            };
         },
         mounted: function () {
-            setInterval(this.refreshTime, 100);
+            this.title = supported_resources[this.idx].title;
+            this.$http.post(supported_resources[this.idx].url + '/fetch', {page_number: 1, page_size: 20}).
+                 then(function (res) {
+                     console.log(res.body);
+                     this.tableData = res.body.results;
+                     if (this.tableData.length > 0 && this.tableData[0].lvid) {
+                         this.isTeacher = true;
+                     }
+                 }).
+                 catch(function (res) {
+                     this.$message(res);
+                 });
         },
         methods: {
-            refreshTime: function () { // 更新显示时间
-                this.cur_time = Date().toLocaleString();
-            }
         },
-        components: {HelloWorld,}
+        components: {}
     };
 </script>
+
+<style>
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 52px;
+        margin: 0 auto;
+        line-height: 300px;
+        text-align: center;
+    }
+
+    .el-carousel__item:nth-child(2n) {
+        /*
+        background-color: #99a9bf;
+        */
+        background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
+    }
+</style>
