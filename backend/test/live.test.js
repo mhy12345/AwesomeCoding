1698bf2d54eea12a2642a905a84b68b55c9975e5
郑逢时 <2016012177@secoder.net>
2018-11-15 13:43:02 +0800

    [add] added test for permission authentication. #95

--- backend/test/live.test.js
+++ backend/test/live.test.js
@@ -4,17 +4,18 @@ const app = require('../app');
 const request = require('supertest-session')(app);
 // 测试初始化
 const should = require('should');
-const { parseText, addUser, clearUser, login, logout, test_user } = require('./test_utils');
+const { parseText, addStudent, addTeacher, clearStudent, clearTeacher, loginStudent, loginTeacher, logout } = require('./test_utils');
+const teacher_operations = ['clear_chat_record', 'block_chatting', 'allow_chatting'];	// 教师权限的操作
 
 var log4js = require("log4js");
 var log4js_config = require("../configures/log.config.js").unittest_configure;
 log4js.configure(log4js_config);
 var logger = log4js.getLogger('test_live');
 
-describe('# Test `live.js`', function () {
+describe('# Test `live.js` as a student', function () {
 	this.timeout(2000);
 	before(function (done) {		// 事先注册用户
-		addUser(request).
+		addStudent(request).
 			then((res) => {
 				done();
 			}).
@@ -23,7 +24,7 @@ describe('# Test `live.js`', function () {
 			});
 	});
 
-	describe('## test login judgement', function () {		// 是否登录的判断
+	describe('## test loginStudent judgement', function () {		// 是否登录的判断
 		it('should fail to load resources when offline.', function (done) {
 			request.
 				get('/api/live/get_cat_record_count').
@@ -40,7 +41,7 @@ describe('# Test `live.js`', function () {
 		});
 		describe('online test', function () {		// 登录状态下的测试
 			before(function (done) {
-				login(request).
+				loginStudent(request).
 					then(done);
 			});
 			it('should fail to load resources when user is not in the class.', function (done) {
@@ -85,7 +86,6 @@ describe('# Test `live.js`', function () {
 							if (err) done(err);
 							let body = parseText(res.text);
 							body.should.have.keys('status', 'results');
-							body.results.should.eql(0);
 							done();
 						});
 				});
@@ -96,11 +96,28 @@ describe('# Test `live.js`', function () {
 						end(function (err, res) {
 							if (err) done(err);
 							let body = parseText(res.text);
-							logger.info('[record]', body);
 							body.should.have.keys('status', 'results');
-							body.results.length.should.eql(0);
 							done();
 						});
+				});
+				teacher_operations.forEach((operation) => {
+					it(`should deny permission when a student tries to \`${operation}\``, function (done) {
+						request.
+							get(`/api/live/${operation}`).
+							query({ course_id: 1 }).
+							end(function (err, res) {
+								if (err) done(err);
+								let body = res.body;
+								body.should.have.keys('status', 'details');
+								body.status.should.eql('FAILED.');
+								body.details.toLowerCase().
+									 should.
+									 containEql('no').
+									 and.
+									 containEql('permission');
+								done();
+							})
+					});
 				});
 				after(function (done) {
 					request.post('/api/class/participants/delete').
@@ -121,7 +138,7 @@ describe('# Test `live.js`', function () {
 	});
 
 	after(function (done) {			// 测试完成后清除用户
-		clearUser(request).
+		clearStudent(request).
 			then((res) => {
 				logger.info('Test over');
 				done();
@@ -131,4 +148,58 @@ describe('# Test `live.js`', function () {
 				done();
 			});
 	});
+});
+
+describe('# Test `live.js` as a teacher', function () {
+	this.timeout(2000);
+	before(function (done) {		// 事先注册用户
+		addTeacher(request).
+			then((res) => {
+				done();
+			}).
+			catch((err) => {
+				done(`Initializing Error ${err}`);
+			});
+	});
+	describe('## online test', function () {
+		before(function (done) {
+			loginTeacher(request).
+				then(done);
+		});
+		it('should reject `/clear_chat_record` when teacher is not in the class', function (done) {	// 教师不在课堂1 中
+			request.get('/api/live/clear_chat_record').
+					query({ course_id: 1 }).
+					end(function (err, res) {
+						if (err) done(err);
+						res.body.should.have.keys('status', 'details');
+						res.body.status.should.eql('FAILED.');
+						res.body.details.toLowerCase().
+							should.
+							containEql('not').
+							and.
+							containEql('in').
+							and.
+							containEql('class');
+						done();
+					});
+		});
+		describe('### teacher join class whose course_id=1', function () {	// 教师加入课堂1
+
+		});
+		after(function (done) {
+			logout(request).
+				then(done);
+		})
+	});
+	after(function (done) {
+		clearTeacher(request).
+			then((res) => {
+				logger.info('Test over');
+				done();
+			}).
+			catch((err) => {
+				logger.info('Test over with', err);
+				done();
+			});
+	})
 });
--- backend/test/test_utils.js
+++ backend/test/test_utils.js
@@ -1,10 +1,20 @@
 // 测试需要用到的函数
 const randomString = require('../utils/funcs').randomString;
-const test_user = {
-	nickname: 'test_name' + randomString(8),
+const test_student = {	// 测试学生账户
+	nickname: 'test_student' + randomString(8),
 	realname: 'TESTER',
 	email: '123456@mail.com',
 	role: 2,
+	motto: 'just for test',
+	password: '111111',
+	phone: '13688880000'
+};
+
+const test_teacher = {	// 测试教师账户
+	nickname: 'test_teacher' + randomString(8),
+	realname: 'TESTER',
+	email: '123456@mail.com',
+	role: 1,
 	motto: 'just for test',
 	password: '111111',
 	phone: '13688880000'
@@ -14,13 +24,13 @@ function parseText(res_text) {		// text 转化为 json
 	return eval('(' + String(res_text) + ')');
 }
 
-function addUser(request) {			// 注册一个新用户
+function _addUser(request, user) {	// 注册用户
 	return new Promise((resolve, reject) => {
 		request.
 			get("/api/developer/do_query?sql=INSERT INTO users " +
-				"(" + Object.keys(test_user).
+				"(" + Object.keys(user).
 							 join(',') + ") VALUES " +
-				"('" + Object.values(test_user).
+				"('" + Object.values(user).
 							  join("','") + "');").
 			end(function (err, res) {
 				if (err) reject(err);
@@ -33,10 +43,18 @@ function addUser(request) {			// 注册一个新用户
 	});
 }
 
-function clearUser(request) {		// 删除用户
+function addStudent(request) {			// 注册一个新学生用户
+	return _addUser(request, test_student);
+}
+
+function addTeacher(request) {		// 注册一个教师账户
+	return _addUser(request, test_teacher);
+}
+
+function _clearUser(request, user) {		// 删除用户
 	return new Promise((resolve, reject) => {
 		request.
-			get("/api/developer/do_query?sql=DELETE FROM users WHERE nickname = '" + test_user.nickname + "'").
+			get("/api/developer/do_query?sql=DELETE FROM users WHERE nickname = '" + user.nickname + "'").
 			end(function (err, res) {
 				if (err) reject(err);
 				resolve(res);
@@ -44,11 +62,19 @@ function clearUser(request) {		// 删除用户
 	});
 }
 
-function login(request) {		// 登录
+function clearStudent(request) {		// 删除学生用户
+	return _clearUser(request, test_student);
+}
+
+function clearTeacher(request) {		// 删除教师账户
+	return _clearUser(request, test_teacher);
+}
+
+function _login(request, user) {		// 登录
 	return new Promise((resolve, reject) => {
 		request.
 			post('/api/user/login').
-			send(test_user).
+			send(user).
 			end(function (err, res) {
 				let body = res.body;
 				if (body.status === 'SUCCESS.')
@@ -59,7 +85,15 @@ function login(request) {		// 登录
 	});
 }
 
-function logout(request) {
+function loginStudent(request) {		// 登录学生
+	return _login(request, test_student);
+}
+
+function loginTeacher(request) {		// 登录教师
+	return _login(request, test_teacher);
+}
+
+function logout(request) {				// 登出当前用户
 	return new Promise((resolve, reject) => {
 		request.
 			get('/api/user/logout').
@@ -74,4 +108,4 @@ function logout(request) {
 	});
 }
 
-module.exports = { parseText, addUser, clearUser, login, logout, test_user };
+module.exports = { parseText, addStudent, addTeacher, clearStudent, clearTeacher, loginStudent, loginTeacher, logout };
