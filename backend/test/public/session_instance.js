413d8db2c486eaa8ffaf083c5afeca18ca7da0cd
郑逢时 <2016012177@secoder.net>
2018-11-15 17:15:49 +0800

    [add] unified entrance of tests that require `supertest-session`. for detailed reasons, see `main_session.test.js`. #95

---
+++ backend/test/public/session_instance.js
@@ -0,0 +1,8 @@
+// instance of `supertest-session`
+
+// 一个没有监听端口的 Express 实例
+const app = require('../../app');
+// Express 实例传入 supertest，使其运行实例
+const request = require('supertest-session')(app);
+
+module.exports = request;
