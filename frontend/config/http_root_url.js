9bee0319ebd3eae1e539247969b753bd3798eb0b
Toby Mao <maohanyang789@163.com>
2018-11-18 20:54:21 +0800

    [hotfix] change http_root_url, #64

--- frontend/config/http_root_url.js
+++ frontend/config/http_root_url.js
@@ -1,4 +1,4 @@
-var root_url = 'http://localhost/';
-// var root_url = 'http://awesomecoding_fantastic67.app.secoder.net/';  todo 发布到 master 时必用这句
+//var root_url = 'http://localhost/';
+var root_url = 'http://awesomecoding_fantastic67.app.secoder.net/'; // todo 发布到 master 时必用这句
 
 module.exports = root_url;
